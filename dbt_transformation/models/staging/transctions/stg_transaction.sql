7c30f2c773f5d343b07d46a09b10bae88b82d46c
melatteshome <melatteshome9@gmail.com>
2025-05-15 03:26:24 +0300

    changed payment status to lowercase

--- dbt_transformation/models/staging/transctions/stg_transaction.sql
+++ dbt_transformation/models/staging/transctions/stg_transaction.sql
@@ -4,13 +4,13 @@ with source as (
 
 renamed as(
     select
-         cast(created_at as timestamp) as created_at,
+        cast(created_at as timestamp) as created_at,
         cast(customer_id as text) as customer_id,
         cast(booking_id as text) as booking_id,
         cast(session_id as text) as session_id,
         cast(product_metadata as text) as product_metadata,
         cast(payment_method as text) as payment_method,
-        cast(payment_status as text) as payment_status,
+        LOWER(cast(payment_status as text)) as payment_status,
         cast(promo_amount as numeric) as promo_amount,
         cast(promo_code as text) as promo_code,
         cast(shipment_fee as numeric) as shipment_fee,
