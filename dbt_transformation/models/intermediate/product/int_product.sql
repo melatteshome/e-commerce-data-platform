48923294b67e2a52d167af8df81861a386426c91
melatteshome <melatteshome9@gmail.com>
2025-05-15 03:24:49 +0300

    year added

--- dbt_transformation/models/intermediate/product/int_product.sql
+++ dbt_transformation/models/intermediate/product/int_product.sql
@@ -15,7 +15,7 @@ enhanced as (
         article_type,
         base_colour,
         season,
-        year,
+        year_time,
         usage_type,
         product_display_name,        
         concat_ws(' - ', master_category, sub_category, article_type) as product_full_category
